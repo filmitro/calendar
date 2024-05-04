b90e9249d4a1f72ff8072bea9245a9d373c0b91d
filmitro <filmitro@gmail.com>
2024-05-04 14:59:57 +0200

    styling

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -18,7 +18,7 @@ const Calendar: React.FC<CalendarProps> = ({ selectedState }) => {
 
     const parseGermanDate = (dateStr: string): Date => {
         const [day, month, year] = dateStr.split('.').map(Number);
-        return new Date(year, month - 1, day);  // Adjusted to ensure it's correctly parsed
+        return new Date(year, month - 1, day + 1);  // Adjusted to ensure it's correctly parsed
     };
 
     const handleDateSelect = (dateStr: string) => {
@@ -75,30 +75,33 @@ const Calendar: React.FC<CalendarProps> = ({ selectedState }) => {
 
     return (
         <div className="md:grid md:grid-cols-2 md:divide-x md:divide-gray-200">
-            <section className="md:pr-14">
-                <h2 className="text-base font-semibold leading-6 text-gray-900">Feiertage in {selectedState}</h2>
-                <ol className="mt-4 space-y-1 text-sm leading-6">
-                    {filteredHolidays.map((holiday, index) => (
-                        <li
-                            key={index}
-                            className={`group flex items-center space-x-4 rounded-xl px-4 py-2 ${
-                                isSelectedHoliday(holiday.date_24) ? 'bg-black' : 'focus-within:bg-gray-100 hover:bg-gray-100'
-                            }`}
-                            onClick={() => handleDateSelect(holiday.date_24)}
-                        >
-                            <div className="flex-auto">
-                                <p className={`${isSelectedHoliday(holiday.date_24) ? 'text-white' : 'text-gray-900'}`}>
-                                    {holiday.name}
-                                </p>
-                                <p className={`mt-0.5 ${isSelectedHoliday(holiday.date_24) ? 'text-white' : 'text-gray-900'}`}>
-                                    {holiday.date_24}
-                                </p>
-                            </div>
-                        </li>
-                    ))}
-                </ol>
-            </section>
+             <section className="md:pr-14">
+            <h2 className="text-base font-semibold bg-white text-gray-900 sticky top-0">
+                {selectedState || "Wählen Sie ein Bundesland aus"}
+            </h2>
+            <ol className="mt-4 space-y-1 text-sm leading-6">
+                {filteredHolidays.map((holiday, index) => (
+                    <li
+                        key={index}
+                        className={`group flex items-center space-x-4 rounded-xl px-4 py-2 ${
+                            isSelectedHoliday(holiday.date_24) ? 'bg-black text-white' : 'focus-within:bg-gray-100 hover:bg-gray-100'
+                        }`}
+                        onClick={() => handleDateSelect(holiday.date_24)}
+                    >
+                        <div className="flex-auto">
+                            <p className={`${isSelectedHoliday(holiday.date_24) ? 'text-white' : 'text-gray-900'}`}>
+                                {holiday.name}
+                            </p>
+                            <p className={`mt-0.5 ${isSelectedHoliday(holiday.date_24) ? 'text-white' : 'text-gray-900'}`}>
+                                {holiday.date_24}
+                            </p>
+                        </div>
+                    </li>
+                ))}
+            </ol>
+        </section>
             <div className="mt-12 md:mt-0 md:pl-14">
+                <div className="sticky top-10">
                 <div className="flex items-center">
                     <h2 className="flex-auto text-sm font-semibold text-gray-900">{`${currentDate.toLocaleString('default', { month: 'long' })} ${currentDate.getFullYear()}`}</h2>
                     <button type="button" className="-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500" onClick={goToPreviousMonth}>
@@ -129,6 +132,7 @@ const Calendar: React.FC<CalendarProps> = ({ selectedState }) => {
                             </button>
                         </div>
                     ))}
+                </div>
                 </div>
             </div>
         </div>
